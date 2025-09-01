0761aa1d1b49443d3c1deef6751896b1b90a6b74
Willian Classen <Wgclassen@gmail.com>
2025-09-01 09:40:45 -0300

    First commit

---
+++ SpeedControl/SpeedControl/Program.cs
@@ -0,0 +1,7 @@
+﻿using SpeedControl;
+
+Vehicle vehicle = new("ABC-1234", 0);
+vehicle.UpdateSpeed(72.5);
+
+Console.WriteLine("Vehicle: " + vehicle.LicensePlate);
+Console.WriteLine("Current speed: " + vehicle.CurrentSpeed + " km/h");
---
+++ SpeedControl/SpeedControl/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace SpeedControl;
+
+public class Vehicle(string licensePlate, double currentSpeed)
+{
+    public string LicensePlate { get; } = licensePlate;
+    public double CurrentSpeed { get; private set; } = currentSpeed;
+
+    public void UpdateSpeed(double newSpeed)
+    {
+        CurrentSpeed = newSpeed;
+    }
+}
