9a214bfda039f3399987db8139f9e19cfd22a471
Willian Classen <Wgclassen@gmail.com>
2025-09-03 16:23:27 -0300

    Salary Adjustment

---
+++ SalaryAdjustment/SalaryAdjustment/Funcionario.cs
@@ -0,0 +1,18 @@
+﻿namespace SalaryAdjustment;
+
+internal class Funcionario(string nome, double salario)
+{
+    public string Nome { get; set; } = nome;
+    public double Salario { get; private set; } = salario;
+    public void ReajustarSalario(double novoValor)
+    {
+        if (novoValor > Salario)
+        {
+            Salario = novoValor;
+        } else
+        {
+            Console.WriteLine("Erro: O novo salário deve ser maior que o atual.");
+        }
+
+    }
+}
---
+++ SalaryAdjustment/SalaryAdjustment/Program.cs
@@ -0,0 +1,6 @@
+﻿using SalaryAdjustment;
+
+Funcionario f = new("Fernanda Lima", 4000);
+f.ReajustarSalario(3500); // Inválido
+f.ReajustarSalario(4200); // Válido
+Console.WriteLine($"Funcionário: {f.Nome}\r\nSalário atual: R$ {f.Salario:F2}");
