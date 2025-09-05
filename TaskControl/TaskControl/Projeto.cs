6ae366610f2b80b02316abcd0c69318074edef13
Willian Classen <Wgclassen@gmail.com>
2025-09-04 21:32:55 -0300

    TaskControl

---
+++ TaskControl/TaskControl/Projeto.cs
@@ -0,0 +1,22 @@
+﻿namespace TaskControl;
+
+internal class Projeto(string nome)
+{
+    public string Nome { get; set; } = nome;
+    private List<string> Tarefas { get; set; } = new List<string>();
+    public void AdicionarTarefa(string tarefa)
+    {
+        Tarefas.Add(tarefa);
+    }
+
+    public void ExibirTarefas()
+    {
+        Console.WriteLine($"Projeto: {Nome}");
+        Console.WriteLine("Tarefas:");
+        foreach (var tarefa in Tarefas)
+        {
+            Console.WriteLine($" - {tarefa}");
+        }
+        Console.WriteLine($"Total: {Tarefas.Count} tarefas");
+    }
+}
